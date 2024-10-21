c1c838303874f8c5cd94e2feb71919b1a2fbd3b8
edward <edwardlfish@gmail.com>
2024-10-21 09:18:37 +0300

    Add accounting

---
+++ src/main/java/ait/cohort46/accounting/dto/UserDto.java
@@ -0,0 +1,21 @@
+package ait.cohort46.accounting.dto;
+
+import java.util.Set;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Singular;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDto {
+	private String login;
+    private String firstName;
+    private String lastName;
+    @Singular
+    private Set<String> roles;
+}
